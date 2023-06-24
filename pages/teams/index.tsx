5fb000cf9ce2308625c1114b422c244b92c204c6
Raşit Çolakel <rasitcolakel@hotmail.com>
2023-06-24 03:46:08 +0300

    Actions column, Team Type and wrong redirect fixed

--- pages/teams/index.tsx
+++ pages/teams/index.tsx
@@ -1,16 +1,25 @@
 import React from "react";
-import { useMany, useTranslate } from "@refinedev/core";
-import { List, useDataGrid, DateField } from "@refinedev/mui";
+import { useTranslate } from "@refinedev/core";
+import {
+  List,
+  useDataGrid,
+  DateField,
+  EditButton,
+  ShowButton,
+  DeleteButton,
+} from "@refinedev/mui";
 import { DataGrid, GridColDef } from "@mui/x-data-grid";
 import { GetServerSideProps } from "next";
 import { authProvider } from "src/authProvider";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import Head from "next/head";
+import { Team } from "src/types";
+import Stack from "@mui/material/Stack";
 
 export default function TeamList() {
   const { dataGridProps } = useDataGrid();
   const t = useTranslate();
-  const columns = React.useMemo<GridColDef<any>[]>(
+  const columns = React.useMemo<GridColDef<Team>[]>(
     () => [
       {
         field: "id",
@@ -22,18 +31,35 @@ export default function TeamList() {
         field: "title",
         headerName: t("pages.teams.fields.title"),
         minWidth: 200,
+        flex: 1,
       },
       {
         field: "description",
         headerName: t("pages.teams.fields.description"),
         minWidth: 200,
+        flex: 1,
       },
       {
-        field: "createdAt",
+        field: "created_at",
         headerName: t("pages.teams.fields.created_at"),
         minWidth: 250,
         renderCell: function render({ value }) {
           return <DateField value={value} />;
+        },
+        flex: 1,
+      },
+      {
+        field: "actions",
+        headerName: t("table.actions"),
+        minWidth: 100,
+        renderCell: function render({ row }) {
+          return (
+            <Stack direction="row">
+              <EditButton size="small" hideText recordItemId={row.id} />
+              <ShowButton size="small" hideText recordItemId={row.id} />
+              <DeleteButton size="small" hideText recordItemId={row.id} />
+            </Stack>
+          );
         },
       },
     ],
@@ -63,7 +89,7 @@ export const getServerSideProps: GetServerSideProps<{}> = async (context) => {
         ...translateProps,
       },
       redirect: {
-        destination: `${redirectTo}?to=${encodeURIComponent("/blog-posts")}`,
+        destination: `${redirectTo}?to=${encodeURIComponent("/teams")}`,
         permanent: false,
       },
     };
