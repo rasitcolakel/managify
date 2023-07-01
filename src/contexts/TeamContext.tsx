e8a4951990c21c792f613fe100ab7d6c80fa1d39
Raşit Çolakel <rasitcolakel@hotmail.com>
2023-07-01 16:44:52 +0300

    Team Context Provider added

---
+++ src/contexts/TeamContext.tsx
@@ -0,0 +1,67 @@
+import {
+  GetOneResponse,
+  INotificationContext,
+  useNotification,
+  useShow,
+  useTranslate,
+} from "@refinedev/core";
+import React from "react";
+import { deleteTeamMember } from "src/services/teams";
+import { TeamMember, TeamWithMembers } from "src/types";
+
+type TeamContextType = {
+  // eslint-disable-next-line no-unused-vars
+  onDelete: (teamMember: TeamMember) => void;
+  data: GetOneResponse<TeamWithMembers> | undefined;
+  queryResult: any;
+  t: any;
+  open: INotificationContext["open"];
+  isLoading: boolean;
+};
+
+export const TeamContext = React.createContext<TeamContextType>(
+  {} as TeamContextType
+);
+
+type TeamContextProviderProps = {
+  children: React.ReactNode;
+};
+const select =
+  "*, owner(*), teamMembers(id, user_id, status, profile:profiles(*))";
+export const TeamContextProvider: React.FC<TeamContextProviderProps> = ({
+  children,
+}) => {
+  const { open } = useNotification();
+  const t = useTranslate();
+  const { queryResult } = useShow<TeamWithMembers>({
+    meta: {
+      select,
+    },
+  });
+  const { data, isLoading } = queryResult;
+  const onDelete = async (teamMember: TeamMember) => {
+    await deleteTeamMember(teamMember.id);
+    if (open) {
+      open({
+        message: t("notifications.success"),
+        type: "success",
+      });
+    }
+    queryResult.refetch();
+  };
+
+  return (
+    <TeamContext.Provider
+      value={{
+        onDelete,
+        data,
+        queryResult,
+        t,
+        isLoading,
+        open,
+      }}
+    >
+      {children}
+    </TeamContext.Provider>
+  );
+};
