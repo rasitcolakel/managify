eebe04a81807d33434438335015e3d5e10348bb2
Raşit Çolakel <rasitcolakel@hotmail.com>
2023-07-09 16:32:33 +0300

    translation props problem fixed

--- pages/teams/[teamId]/tasks/create/index.tsx
+++ pages/teams/[teamId]/tasks/create/index.tsx
@@ -1,5 +1,8 @@
 import { TaskCreate } from "@components/teams/tasks/create";
+import { GetServerSideProps } from "next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import React from "react";
+import { authProvider } from "src/authProvider";
 
 type Props = {};
 
@@ -10,3 +13,29 @@ export default function index({}: Props) {
     </div>
   );
 }
+
+export const getServerSideProps: GetServerSideProps<{}> = async (context) => {
+  const { authenticated, redirectTo } = await authProvider.check(context);
+
+  const translateProps = await serverSideTranslations(context.locale ?? "en", [
+    "common",
+  ]);
+
+  if (!authenticated) {
+    return {
+      props: {
+        ...translateProps,
+      },
+      redirect: {
+        destination: `${redirectTo}?to=${encodeURIComponent("/teams")}`,
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {
+      ...translateProps,
+    },
+  };
+};
