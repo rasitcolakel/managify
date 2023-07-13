40b0deef53ab46742e826edabb7d09c68fd2f381
Raşit Çolakel <rasitcolakel@hotmail.com>
2023-07-14 00:57:48 +0300

    Titles added for Login, Register and Forgot Password

--- pages/forgot-password/index.tsx
+++ pages/forgot-password/index.tsx
@@ -1,45 +1,53 @@
-import {AuthPage, ThemedTitleV2} from "@refinedev/mui";
+import { useTranslate } from "@refinedev/core";
+import { AuthPage, ThemedTitleV2 } from "@refinedev/mui";
 
-import {GetServerSideProps} from "next";
-import {serverSideTranslations} from "next-i18next/serverSideTranslations";
+import { GetServerSideProps } from "next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import Head from "next/head";
 
-import {authProvider} from "src/authProvider";
+import { authProvider } from "src/authProvider";
 
-import {AppIcon} from "src/components/app-icon";
+import { AppIcon } from "src/components/app-icon";
 
 export default function ForgotPassword() {
-    return (
-        <AuthPage
-            type="forgotPassword"
-            title={
-                <ThemedTitleV2 collapsed={false} text="Managify" icon={<AppIcon/>}/>
-            }
-        />
-    );
+  const t = useTranslate();
+  return (
+    <main>
+      <Head>
+        <title>{t("documentTitle.auth.forgotPassword")}</title>
+      </Head>
+      <AuthPage
+        type="forgotPassword"
+        title={
+          <ThemedTitleV2 collapsed={false} text="Managify" icon={<AppIcon />} />
+        }
+      />
+    </main>
+  );
 }
 
 ForgotPassword.noLayout = true;
 
 export const getServerSideProps: GetServerSideProps<{}> = async (context) => {
-    const {authenticated} = await authProvider.check(context);
+  const { authenticated } = await authProvider.check(context);
 
-    const translateProps = await serverSideTranslations(context.locale ?? "en", [
-        "common",
-    ]);
+  const translateProps = await serverSideTranslations(context.locale ?? "en", [
+    "common",
+  ]);
 
-    if (authenticated) {
-        return {
-            props: {},
-            redirect: {
-                destination: `/`,
-                permanent: false,
-            },
-        };
-    }
-
+  if (authenticated) {
     return {
-        props: {
-            ...translateProps,
-        },
+      props: {},
+      redirect: {
+        destination: `/`,
+        permanent: false,
+      },
     };
+  }
+
+  return {
+    props: {
+      ...translateProps,
+    },
+  };
 };
--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,45 +1,53 @@
-import {AuthPage, ThemedTitleV2} from "@refinedev/mui";
+import { useTranslate } from "@refinedev/core";
+import { AuthPage, ThemedTitleV2 } from "@refinedev/mui";
 
-import {GetServerSideProps} from "next";
-import {serverSideTranslations} from "next-i18next/serverSideTranslations";
+import { GetServerSideProps } from "next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import Head from "next/head";
 
-import {authProvider} from "src/authProvider";
+import { authProvider } from "src/authProvider";
 
-import {AppIcon} from "src/components/app-icon";
+import { AppIcon } from "src/components/app-icon";
 
 export default function Login() {
-    return (
-        <AuthPage
-            type="login"
-            title={
-                <ThemedTitleV2 collapsed={false} text="Managify" icon={<AppIcon/>}/>
-            }
-        />
-    );
+  const t = useTranslate();
+  return (
+    <main>
+      <Head>
+        <title>{t("documentTitle.auth.login")}</title>
+      </Head>
+      <AuthPage
+        type="login"
+        title={
+          <ThemedTitleV2 collapsed={false} text="Managify" icon={<AppIcon />} />
+        }
+      />
+    </main>
+  );
 }
 
 Login.noLayout = true;
 
 export const getServerSideProps: GetServerSideProps<{}> = async (context) => {
-    const {authenticated} = await authProvider.check(context);
+  const { authenticated } = await authProvider.check(context);
 
-    const translateProps = await serverSideTranslations(context.locale ?? "en", [
-        "common",
-    ]);
+  const translateProps = await serverSideTranslations(context.locale ?? "en", [
+    "common",
+  ]);
 
-    if (authenticated) {
-        return {
-            props: {},
-            redirect: {
-                destination: `/`,
-                permanent: false,
-            },
-        };
-    }
-
+  if (authenticated) {
     return {
-        props: {
-            ...translateProps,
-        },
+      props: {},
+      redirect: {
+        destination: `/`,
+        permanent: false,
+      },
     };
+  }
+
+  return {
+    props: {
+      ...translateProps,
+    },
+  };
 };
